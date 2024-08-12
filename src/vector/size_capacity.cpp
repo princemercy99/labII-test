24568a21da9f883a78693e3de7b17b7a53c568a6
Prince-Mercy Habtemariam <finalshow64@gmail.com>
2024-08-12 21:18:12 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -9,14 +9,20 @@ using namespace std;
 int main()
 {
     vector<int> vec;
-    cout << "vec: size: " << vec.size() 
-         << " capacity: " << vec.capacity() << endl;
+    cout << "vec: size: " << vec.size() // size refers to the number of elements currently in the vector
+         << " capacity: " << vec.capacity() << endl; // capacity refers to the amount of memory that has been allocated for the vector
 
-    for(int i = 0; i < 24; i++) {
+    for (int i = 0; i < 24; i++)
+    {
         vec.push_back(i);
-        cout << "vec: size: " << vec.size() 
+        cout << "vec: size: " << vec.size()
              << " capacity: " << vec.capacity() << endl;
     }
 
+/* If the vectors size equals the capacity, then eveytime there is a new element inserted, new memory is allocated , and the
+vector then has to copy all those elements to the new location then add the element. This can take O(n) time.
+Hence ocassionally doubling the capacity of the vector as it reaches full capacity occasionaly takes O(n) time
+but has an average of O(1) time since all that has to be done most of the time are insertions. Therefore the capacity
+grows with size since this is less computationally expensive.*/
     return 0;
 }
